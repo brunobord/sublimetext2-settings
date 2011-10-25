eda503d875fd725083b3b23a9f9b0f16cd3e088a
Bruno Bord <brunobord@gmail.com>
2011-10-26 00:27:35 +0200

    update of word_count.py

--- word_count.py
+++ word_count.py
@@ -2,11 +2,12 @@ import re
 import sublime
 import sublime_plugin
 
+
 class WordCountCommand(sublime_plugin.TextCommand):
 
-   def run(self, edit, args=None):
-       content = self.view.substr(sublime.Region(0, self.view.size()))
-       sublime.status_message('%d characters / %d words in text' % self.word_count(content))
+    def run(self, edit, args=None):
+        content = self.view.substr(sublime.Region(0, self.view.size()))
+        sublime.status_message('%d characters / %d words in text' % self.word_count(content))
 
-   def word_count(self, text):
-       return len(text), len(re.findall('(\w+)', text))
+    def word_count(self, text):
+        return len(text), len(re.compile(r'(\w+)', re.U).findall(text))
